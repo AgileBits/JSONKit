5fdc92eb945d28508db9087ebb9157bf541fd07a
Roustem Karimov <roustem@agilebits.com>
2012-07-18 11:08:42 -0400

    Adding Xcode project to build static libraries for iOS 6 and OS X 10.7.

---
+++ JSONKitOSX/JSONKitOSX.h
@@ -0,0 +1,13 @@
+//
+//  JSONKitOSX.h
+//  JSONKitOSX
+//
+//  Created by Roustem Karimov on 2012-07-18.
+//  Copyright (c) 2012 AgileBits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONKitOSX : NSObject
+
+@end
---
+++ JSONKitOSX/JSONKitOSX.m
@@ -0,0 +1,13 @@
+//
+//  JSONKitOSX.m
+//  JSONKitOSX
+//
+//  Created by Roustem Karimov on 2012-07-18.
+//  Copyright (c) 2012 AgileBits. All rights reserved.
+//
+
+#import "JSONKitOSX.h"
+
+@implementation JSONKitOSX
+
+@end
